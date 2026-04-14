2fa5646a86713150bcb99b186e05c21904146185
Daniel Lemire <daniel@lemire.me>
2026-04-14 10:52:06 -0400

    adding.

---
+++ 2026/04/14/src/main3.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+
+int main() {
+    for(double i = 0; i <= 10; i += 0.31232) {
+        printf("%.6f\n", i);
+    }
+    return 0;
+}
---
+++ 2026/04/14/src/main4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    for(double i = 0; i <= 10; i += 0.31232) {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
